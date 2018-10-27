70a798d9b845bdb8105aa12a4af617a9dcff7f14
Boyd Ching <chingboyd@gmail.com>
2018-10-27 18:44:50 +0700

    add changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+// function TableRow() {
+//   return ()
+// }
+
+class TableContents extends Component {
+  // renderRow() {
+  //   return(
+  //     <TableRow
+  //       onClick={() => expand}
+  //     />
+  //   );
+  // }
+
+  render() {
+    return(
+      <div>
+        <div className="content-row">
+          <span>Earthquake</span>
+          <div className="content-row-img">
+            <img alt="" src="/imgs/earthquake.jpeg"/>
+          </div>
+        </div>
+        <div className="content-row">
+          <span>Volcano</span>
+          <div className="content-row-img">
+            <img alt="" src="/imgs/volcano.png"/>
+          </div>
+        </div>
+        <div className="content-row">
+          <span>Landslide</span>
+          <div className="content-row-img">
+            <img alt="" src="/imgs/landslide.png"/>
+          </div>
+        </div>
+        <div className="content-row">
+          <span>Tsunami</span>
+          <div className="content-row-img">
+            <img alt="" src="/imgs/tsunami.png"/>
+          </div>
+        </div>
+        <div className="content-row">
+          <span>About</span>
+        </div>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="landing-page">
+        <div className="header">
+          <h1>Japan Disaster Handbook</h1>
+        </div>
+        <div className="table-contents-rows">
+          <TableContents />
+        </div>
       </div>
     );
   }
