da71a0cbdb1986a79ec81fbf9dae9724da5986d7
Boyd Ching <chingboyd@gmail.com>
2018-10-27 19:03:04 +0700

    Updates

---
+++ precache-manifest.2e1f66b51229192ef56039b0c9c58f2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e04b2d66826c091ff56",
+    "url": "/jets-disaster-manual/static/js/runtime~main.7e04b2d6.js"
+  },
+  {
+    "revision": "b529fb9914ac30399636",
+    "url": "/jets-disaster-manual/static/js/main.b529fb99.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/jets-disaster-manual/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "b529fb9914ac30399636",
+    "url": "/jets-disaster-manual/static/css/main.aafc9776.chunk.css"
+  },
+  {
+    "revision": "29d304b4b2fa1cd1aa9c0b397f523a24",
+    "url": "/jets-disaster-manual/index.html"
+  }
+];
