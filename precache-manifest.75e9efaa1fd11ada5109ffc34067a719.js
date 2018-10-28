3157665548b464fef96be81b91d131282880fd85
Boyd Ching <chingboyd@gmail.com>
2018-10-29 04:52:35 +0700

    Updates

--- precache-manifest.75e9efaa1fd11ada5109ffc34067a719.js
+++ precache-manifest.75e9efaa1fd11ada5109ffc34067a719.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/jets-disaster-manual/static/js/runtime~main.7e04b2d6.js"
   },
   {
-    "revision": "010acde3aeb03880f791",
-    "url": "/jets-disaster-manual/static/js/main.010acde3.chunk.js"
+    "revision": "6e86bb52e4d6f2f94492",
+    "url": "/jets-disaster-manual/static/js/main.6e86bb52.chunk.js"
   },
   {
     "revision": "6eb6405c9d99258624bc",
     "url": "/jets-disaster-manual/static/js/1.6eb6405c.chunk.js"
   },
   {
-    "revision": "010acde3aeb03880f791",
+    "revision": "6e86bb52e4d6f2f94492",
     "url": "/jets-disaster-manual/static/css/main.5bc1aaaa.chunk.css"
   },
   {
-    "revision": "d748d581a85a08786925d385d4310f38",
+    "revision": "840577c680ac7703370d03f4c1e87e08",
     "url": "/jets-disaster-manual/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jets-disaster-manual/precache-manifest.dbef6e9ded8368e7d38c1c9fbf519e16.js"
+  "/jets-disaster-manual/precache-manifest.75e9efaa1fd11ada5109ffc34067a719.js"
 );
 
 workbox.clientsClaim();
