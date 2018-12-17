4c5487cb75830d05171e552a4027c61e6cdbb65f
Boyd Ching <chingboyd@gmail.com>
2018-12-17 12:56:01 -0900

    refactor repeated render code into function

--- src/content/earthquakeContent.js
+++ src/content/earthquakeContent.js
@@ -2,6 +2,39 @@ import React, {Component} from 'react';
 
 import earthquakeText from './json/earthquakeText.json';
 
+const reportVocab = earthquakeText.reportVocab;
+const tvVocabJson = earthquakeText.tvRikai.vocab;
+
+const tvPhraseJson = earthquakeText.tvRikai.phrase;
+const tvPhraseArr = [];
+Object.keys(tvPhraseJson).forEach(key => {
+  tvPhraseArr.push(tvPhraseJson[key]);
+});
+
+const scenarioTwoVocab = earthquakeText.scenario2.vocab;
+const scenarioTwoPhrases = earthquakeText.scenario2.phrase;
+
+function RenderObjectsOfList(props) {
+  const scen2Arr = Object.keys(props.content).map((key, index) => {
+    let wordList = [];
+    wordList = props.content[key].map((word, index) => {
+      return( <div key={index}>{word}</div> );
+    });
+
+    return(
+        <div className={props.className} key={index}>
+          {wordList}
+        </div>
+      );
+  });
+
+  return(
+    <div className="vocab-grid">
+      {scen2Arr}
+    </div>
+  );
+}
+
 class EarthquakeContent extends Component {
   render() {
     let toggle = "";
@@ -10,10 +43,9 @@ class EarthquakeContent extends Component {
     }
 
     return (
-
       <div className={"sub-row" + toggle}>
-        <h2>自分を守る編</h2>
         <div className="chapter-content">
+          <h2>自分を守る編</h2>
 
           <p className="static-box round-border blue-border">{earthquakeText.goal}</p><br/>
 
@@ -35,75 +67,71 @@ class EarthquakeContent extends Component {
           <p className="red-thin-round-border light-red-background">{earthquakeText.youtubeBox.emergencyInfo}<br/>
             <b>{earthquakeText.youtubeBox.linkOne.leadText}</b><a href={earthquakeText.youtubeBox.linkOne.link}>{earthquakeText.youtubeBox.linkOne.link}</a><br/>
             <b>{earthquakeText.youtubeBox.linkTwo.leadText}</b><a href={earthquakeText.youtubeBox.linkTwo.link}>{earthquakeText.youtubeBox.linkTwo.link}</a></p>
-        </div>
 
-        <div className="flex-vertical">
-          <p className="static-box round-border light-green-border light-green-background">{earthquakeText.tvScenario.one}</p>
-          <p className="static-box round-border dark-red-border">{earthquakeText.tvScenario.two}</p>
-        </div>
-        <p className="static-box center-text square-border light-green-border">{earthquakeText.tvScenario.three}</p><br/>
+            <div className="flex-vertical">
+              <p className="static-box round-border light-green-border light-green-background">{earthquakeText.tvScenario.one}</p>
+              <p className="static-box round-border dark-red-border">{earthquakeText.tvScenario.two}</p>
+            </div>
 
-        <div className="vocab-grid">
-          <div className="round-border light-green-background">
-            {earthquakeText.reportVocab.earthquake.eng}<br/>
-            {earthquakeText.reportVocab.earthquake.say}<br/>
-            {earthquakeText.reportVocab.earthquake.hira}<br/>
-            {earthquakeText.reportVocab.earthquake.kanji}
-          </div>
-          <div className="round-border light-green-background">
-            {earthquakeText.reportVocab.intensity.eng}<br/>
-            {earthquakeText.reportVocab.intensity.say}<br/>
-            {earthquakeText.reportVocab.intensity.hira}<br/>
-            {earthquakeText.reportVocab.intensity.kanji}
-          </div>
-          <div className="round-border light-green-background">
-            {earthquakeText.reportVocab.magnitude.eng}<br/>
-            {earthquakeText.reportVocab.magnitude.say}<br/>
-            {earthquakeText.reportVocab.magnitude.hira}<br/>
-            {earthquakeText.reportVocab.magnitude.kanji}
-          </div>
-          <div className="round-border light-green-background">
-            {earthquakeText.reportVocab.epicenter.eng}<br/>
-            {earthquakeText.reportVocab.epicenter.say}<br/>
-            {earthquakeText.reportVocab.epicenter.hira}<br/>
-            {earthquakeText.reportVocab.epicenter.kanji}
-          </div>
-          <div className="round-border light-green-background">
-            {earthquakeText.reportVocab.countOne.one}<br/>
-            {earthquakeText.reportVocab.countOne.two}<br/>
-            {earthquakeText.reportVocab.countOne.three}<br/>
-            {earthquakeText.reportVocab.countOne.four}<br/>
-            {earthquakeText.reportVocab.countOne.five}
-          </div>
-          <div className="round-border light-green-background">
-            {earthquakeText.reportVocab.countTwo.six}<br/>
-            {earthquakeText.reportVocab.countTwo.seven}<br/>
-            {earthquakeText.reportVocab.countTwo.eight}<br/>
-            {earthquakeText.reportVocab.countTwo.nine}
-          </div>
-          <div className="round-border light-green-background">
-            {earthquakeText.reportVocab.countAmp.fiveMin}<br/>
-            {earthquakeText.reportVocab.countAmp.fiveMax}<br/>
-            {earthquakeText.reportVocab.countAmp.sixMin}<br/>
-            {earthquakeText.reportVocab.countAmp.sixMax}
-          </div>
-          <div className="round-border light-green-background">
-            {earthquakeText.reportVocab.countDec.one}<br/>
-            {earthquakeText.reportVocab.countDec.two}<br/>
-            {earthquakeText.reportVocab.countDec.three}
-          </div>
-        </div>
+            <p className="static-box center-text square-border light-green-border">{earthquakeText.tvScenario.three}</p><br/>
 
-        <p className="round-border dark-red-border">
-          <b>{earthquakeText.definitionRed.mag}</b>{earthquakeText.definitionRed.magnitudeDef}<br/>
-          <b>{earthquakeText.definitionRed.shindo}</b>{earthquakeText.definitionRed.shindoDef}<br/>
-          <a href={earthquakeText.definitionRed.link}>{earthquakeText.definitionRed.link}</a>
-        </p>
+            <RenderObjectsOfList
+              content={reportVocab}
+              className="round-border light-green-background"></RenderObjectsOfList>
 
-        <div className="flex-row">
-          <p className="round-border red-background">{earthquakeText.eqRecordRow.kobe}</p>
-          <p className="round-border red-background">{earthquakeText.eqRecordRow.tohoku}</p>
-          <p className="round-border red-background">{earthquakeText.eqRecordRow.kumamoto}</p>
+            <p className="round-border dark-red-border">
+              <b>{earthquakeText.definitionRed.mag}</b>{earthquakeText.definitionRed.magnitudeDef}<br/>
+              <b>{earthquakeText.definitionRed.shindo}</b>{earthquakeText.definitionRed.shindoDef}<br/>
+              <a href={earthquakeText.definitionRed.link}>{earthquakeText.definitionRed.link}</a>
+            </p>
+
+            <div className="flex-row">
+              <p className="round-border red-background">{earthquakeText.eqRecordRow.kobe}</p>
+              <p className="round-border red-background">{earthquakeText.eqRecordRow.tohoku}</p>
+              <p className="round-border red-background">{earthquakeText.eqRecordRow.kumamoto}</p>
+            </div>
+
+            <p className="static-box center-text square-border light-green-border">{earthquakeText.tvRikai.header}</p><br/>
+
+            <RenderObjectsOfList
+              content={tvVocabJson}
+              className="round-border light-green-background"
+            />
+
+            <RenderObjectsOfList
+              content={tvPhraseJson}
+              className="round-border salmon-background"
+            />
+
+            <p className="red-thin-round-border red-background">
+              <b>Quick notes:</b><br/>
+              {earthquakeText.quickNotes.point1}<br/>
+              {earthquakeText.quickNotes.point2}<br/>
+              {earthquakeText.quickNotes.point3}
+            </p>
+
+            <h2>自分を守る編（２）</h2>
+            <p className="round-border light-brown-border light-brown-background">
+              {earthquakeText.scenario2.one}<br/>
+              {earthquakeText.scenario2.two}
+            </p>
+            <p className="round-border dark-red-border">
+              {earthquakeText.scenario2.action}
+            </p>
+            <p className="square-border light-green-border">
+              {earthquakeText.scenario2.vocabHeader}
+            </p>
+
+            <RenderObjectsOfList
+              content={scenarioTwoVocab}
+              className="round-border light-green-background"
+            />
+
+            <RenderObjectsOfList
+              content={scenarioTwoPhrases}
+              className="round-border salmon-background"
+            />
+
         </div>
       </div>
     );
